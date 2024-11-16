c7d59e92a5db67042b0a0b93b3256967faa77b81
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2024-11-16 12:16:21 -0500

    Update referralnu_ddl.sql

--- referralnu_ddl.sql
+++ referralnu_ddl.sql
@@ -3,79 +3,114 @@ CREATE DATABASE IF NOT EXISTS referralnu;
 USE referralnu;
 
 -- Create Advisor table
-CREATE TABLE Advisor (
-  advisorID INT PRIMARY KEY,
-  name VARCHAR(255),
-  contactInfo TEXT,
-  college VARCHAR(255)
+CREATE TABLE Advisor
+(
+    advisorID   INT PRIMARY KEY AUTO_INCREMENT,
+    firstName   VARCHAR(20)  NOT NULL,
+    lastName    VARCHAR(40)  NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    phoneNumber VARCHAR(20),
+    college     VARCHAR(255) NOT NULL
 );
 
 -- Create Students table
-CREATE TABLE Students (
-  studentId INT PRIMARY KEY,
-  name VARCHAR(255),
-  contactInfo TEXT,
-  advisorId INT,
-  FOREIGN KEY (advisorId) REFERENCES Advisor(advisorID)
+CREATE TABLE Students
+(
+    studentId   INT PRIMARY KEY AUTO_INCREMENT,
+    firstName   VARCHAR(20)  NOT NULL,
+    lastName    VARCHAR(40)  NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    phoneNumber VARCHAR(20),
+    advisorId   INT,
+    FOREIGN KEY (advisorId) REFERENCES Advisor (advisorID)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
 -- Create Admin table
-CREATE TABLE Admin (
-  adminId INT PRIMARY KEY,
-  name VARCHAR(255),
-  contactInfo TEXT
+CREATE TABLE Admin
+(
+    adminId     INT PRIMARY KEY AUTO_INCREMENT,
+    firstName   VARCHAR(20)  NOT NULL,
+    lastName    VARCHAR(40)  NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    phoneNumber VARCHAR(20)
 );
 
 -- Create Industries table
-CREATE TABLE Industries (
-  industryId INT PRIMARY KEY,
-  name VARCHAR(255)
+CREATE TABLE Industries
+(
+    industryId INT PRIMARY KEY NOT NULL,
+    name       VARCHAR(255)    NOT NULL
 );
 
 -- Create Referrer table
-CREATE TABLE Referrer (
-  referrerId INT PRIMARY KEY,
-  name VARCHAR(255),
-  contactInfo TEXT,
-  creationDate TIMESTAMP,
-  updateDate TIMESTAMP,
-  company VARCHAR(255),
-  adminId INT,
-  industryId INT,
-  FOREIGN KEY (adminId) REFERENCES Admin(adminId),
-  FOREIGN KEY (industryId) REFERENCES Industries(industryId)
+CREATE TABLE Referrer
+(
+    referrerId   INT PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(255) NOT NULL,
+    email        VARCHAR(255) NOT NULL,
+    phoneNumber  VARCHAR(20),
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateDate   TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    company      VARCHAR(255) NOT NULL,
+    adminId      INT          NOT NULL,
+    industryId   INT          NOT NULL,
+    numReferrals INT,
+    FOREIGN KEY (adminId) REFERENCES Admin (adminId)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (industryId) REFERENCES Industries (industryId)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
 -- Create Connections table
-CREATE TABLE Connections (
-  connectionId INT PRIMARY KEY,
-  referrerId INT,
-  creationDate TIMESTAMP,
-  studentId INT,
-  FOREIGN KEY (referrerId) REFERENCES Referrer(referrerId),
-  FOREIGN KEY (studentId) REFERENCES Students(studentId)
+CREATE TABLE Connections
+(
+    connectionId INT NOT NULL,
+    PRIMARY KEY connectionId (referrerId, studentId),
+    referrerId   INT NOT NULL,
+    creationDate TIMESTAMP,
+    studentId    INT NOT NULL,
+    FOREIGN KEY (referrerId) REFERENCES Referrer (referrerId)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    FOREIGN KEY (studentId) REFERENCES Students (studentId)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
 -- Create Requests table
-CREATE TABLE Requests (
-  studentId INT,
-  referrerId INT,
-  status VARCHAR(50),
-  requestDate TIMESTAMP,
-  industryId INT,
-  PRIMARY KEY (studentId, referrerId),
-  FOREIGN KEY (studentId) REFERENCES Students(studentId),
-  FOREIGN KEY (referrerId) REFERENCES Referrer(referrerId),
-  FOREIGN KEY (industryId) REFERENCES Industries(industryId)
+CREATE TABLE Requests
+(
+    studentId   INT NOT NULL,
+    referrerId  INT NOT NULL,
+    status      VARCHAR(50),
+    requestDate TIMESTAMP,
+    industryId  INT NOT NULL,
+    createdAt   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (studentId, referrerId),
+    FOREIGN KEY (studentId) REFERENCES Students (studentId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (industryId) REFERENCES Industries (industryId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 -- Create Messages table
-CREATE TABLE Messages (
-  messageId INT PRIMARY KEY,
-  messageContent TEXT,
-  adminId INT,
-  sentAt TIMESTAMP,
-  connectionId INT,
-  FOREIGN KEY (adminId) REFERENCES Admin(adminId),
-  FOREIGN KEY (connectionId) REFERENCES Connections(connectionId)
+CREATE TABLE Messages
+(
+    messageId      INT PRIMARY KEY NOT NULL,
+    messageContent TEXT            NOT NULL,
+    adminId        INT             NOT NULL,
+    sentAt         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    connectionId   INT             NOT NULL,
+    FOREIGN KEY (adminId) REFERENCES Admin (adminId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (connectionId) REFERENCES Connections (connectionId)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
