083c1c04597282891aa2c4fe662ef87807435781
ishasakamuri <sakamuri.i@northeastern.edu>
2024-12-04 22:58:47 -0500

    Update Referrer_Home.py

--- app/src/pages/Referrer_Home.py
+++ app/src/pages/Referrer_Home.py
@@ -14,35 +14,29 @@ st.write('')
 st.write('')
 st.write('### What would you like to do?')
 
-# 4.1
+if st.button('See All Requests',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/referrer_all_reqs.py')
+
+# 4.1, 4.4
 if st.button('See Student Information',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/referrer_all_resumes.py')
 
 # 4.2
-if st.button('Remove Companies',
+if st.button('Manage Companies',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/referrer_remove_company.py')
 
-# 4.3
-if st.button("Change Application Status",
+# 4.3, 4.5
+if st.button("View and Change Application Status",
              type='primary',
              use_container_width=True):
   st.switch_page('pages/referrer_app_status.py')
 
-# 4.4
-if st.button("Communicate Requirements",
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/referrer_cons.py')
-
-# 4.5
-if st.button("Student Referral Status",
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/referrer_student_status.py')
 
 # 4.6
 if st.button("Edit Contact Information",
