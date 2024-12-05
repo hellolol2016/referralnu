6b1dfbefabe14c60f5ed9ea6da8672fadcddf959
andreashsef <132906000+andreashsef@users.noreply.github.com>
2024-12-05 16:33:58 -0500

    remvoed sample compnay route

--- api/backend/companies/company_routes.py
+++ api/backend/companies/company_routes.py
@@ -1,7 +1,4 @@
-########################################################
-# Sample customers blueprint of endpoints
-# Remove this file if you are not using it in your project
-########################################################
+
 from flask import Blueprint
 from flask import request
 from flask import jsonify
@@ -14,18 +11,6 @@ from backend.db_connection import db
 # routes.
 companies = Blueprint('companies', __name__)
 
-#------------------------------------------------------------
-# Get all customers from the system
-@companies.route('/<companyId>', methods=['GET'])
-def get_customers(companyId):
-
-    cursor = db.get_db().cursor()
-    cursor.execute('''SELECT * FROM Companies WHERE companyId = %s''', (companyId,))
-    theData = cursor.fetchall()
-    
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
-    return the_response
 
 
 @companies.route('/<int:company_id>', methods=['DELETE'])
