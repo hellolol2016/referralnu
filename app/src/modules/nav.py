10d85fa94c1e379feb18d217546f193e63272f7a
andreashsef <132906000+andreashsef@users.noreply.github.com>
2024-12-05 15:49:34 -0500

    removed sample sidebar

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -15,38 +15,6 @@ def AboutPageNav():
     st.sidebar.page_link("Home.py", label = "Home", icon = "🏠")
 
 
-#### ------------------------ Examples for Role of pol_strat_advisor ------------------------
-def PolStratAdvHomeNav():
-    st.sidebar.page_link(
-        "pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon="👤"
-    )
-
-
-def WorldBankVizNav():
-    st.sidebar.page_link(
-        "pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon="🏦"
-    )
-
-
-def MapDemoNav():
-    st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon="🗺️")
-
-
-## ------------------------ Examples for Role of usaid_worker ------------------------
-def ApiTestNav():
-    st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon="🛜")
-
-
-def PredictionNav():
-    st.sidebar.page_link(
-        "pages/11_Prediction.py", label="Regression Prediction", icon="📈"
-    )
-
-
-def ClassificationNav():
-    st.sidebar.page_link(
-        "pages/13_Classification.py", label="Classification Demo", icon="🌺"
-    )
 
 
 #### ------------------------ System Admin Role ------------------------
@@ -63,7 +31,6 @@ def AdvisorPageNav():
     st.sidebar.page_link("pages/Advisor_Referral_Progress.py", label = "Progress")
     st.sidebar.page_link("pages/Advisor_Results.py", label = "Results")
     st.sidebar.page_link("pages/Advisor_Student_Info.py", label = "Student Info")
-    st.sidebar.page_link("pages/Advisor_followup.py", label = "Follow Up date")
 
 
 def ReferralSeekerPageNav():
@@ -75,7 +42,6 @@ def ReferrerPageNav():
     st.sidebar.page_link("pages/Referrer_Home.py", label = "Referrer")
     st.sidebar.page_link("pages/referrer_remove_company.py", label = "Change Employment")
     st.sidebar.page_link("pages/referrer_all_resumes.py", label = "Resumes")
-    st.sidebar.page_link("pages/referrer_app_status.py", label="Application Status")
 
 
 # --------------------------------Links Function -----------------------------------------------
@@ -97,17 +63,6 @@ def SideBarLinks():
     # Show the other page navigators depending on the users' role.
     if st.session_state["authenticated"]:
 
-        # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
-        if st.session_state["role"] == "pol_strat_advisor":
-            PolStratAdvHomeNav()
-            WorldBankVizNav()
-            MapDemoNav()
-
-        # If the user role is usaid worker, show the Api Testing page
-        if st.session_state["role"] == "usaid_worker":
-            PredictionNav()
-            ApiTestNav()
-            ClassificationNav()
 
         # If the user is an administrator, give them access to the administrator pages
         if st.session_state["role"] == "administrator":
